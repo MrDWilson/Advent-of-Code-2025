08b4101fb5996150aa28c19401373bfbbdca24bc
Danny Wilson <Danny.Wilson@celaton.com>
2025-12-01 09:01:30 +0000

    feat: Day 1 part 2 solution

--- dotnet/Solutions/Day01/Day01.cs
+++ dotnet/Solutions/Day01/Day01.cs
@@ -13,23 +13,39 @@ public class Day01(IFileLoader fileLoader, IOptions<SolutionOptions> options) : 
         var lines = await LoadLines<string>();
         var rotations = lines.Select(line => (Direction: line.First(), Count: int.Parse(new string([.. line.Skip(1)]))));
 
-        var countOfZeros = 0;
+        var countOfZerosPartOne = 0;
+        var countOfZerosPartTwo = 0;
         var location = 50;
         foreach (var rotation in rotations)
         {
-            location = rotation.Direction switch {
+            (location, int zeroCount) = rotation.Direction switch {
                 'R' => TurnRight(rotation.Count, location),
                 'L' => TurnLeft(rotation.Count, location),
                 _ => throw new ArgumentException(nameof(rotation.Direction))
             };
 
             if (location is 0)
-                countOfZeros++;
+                countOfZerosPartOne++;
+
+            countOfZerosPartTwo += zeroCount;
         }
 
-        return countOfZeros.ToString();
+        return CurrentOptions.SolutionType is SolutionType.First ? countOfZerosPartOne.ToString() : countOfZerosPartTwo.ToString();
     }
 
-    int TurnLeft(int turn, int location) => turn is 0 ? location : TurnLeft(--turn, location is 0 ? 99 : --location);
-    int TurnRight(int turn, int location) => turn is 0 ? location : TurnRight(--turn, location is 99 ? 0 : ++location);
+    static (int Location, int ZeroCount) TurnLeft(int turn, int location, int countZeros = 0) 
+    {
+        if (location is 0 && turn is not 0)
+            countZeros++;
+
+        return turn is 0 ? (location, countZeros) : TurnLeft(--turn, location is 0 ? 99 : --location, countZeros);
+    }
+
+    static (int Location, int ZeroCount) TurnRight(int turn, int location, int countZeros = 0) 
+    {
+        if (location is 0 && turn is not 0)
+            countZeros++;
+
+        return turn is 0 ? (location, countZeros) : TurnRight(--turn, location is 99 ? 0 : ++location, countZeros);
+    }
 }
