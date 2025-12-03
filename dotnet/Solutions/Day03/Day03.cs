273cd9251b276bd7c32d441b04222a3067aa0b49
Danny Wilson <Danny.Wilson@celaton.com>
2025-12-03 09:57:44 +0000

    feat: Day 3 both solutions

--- dotnet/Solutions/Day03/Day03.cs
+++ dotnet/Solutions/Day03/Day03.cs
@@ -10,6 +10,46 @@ public class Day03(IFileLoader fileLoader, IOptions<SolutionOptions> options) : 
 
     public override async Task<string> Solve()
     {
-        return "Hello, World!";
+        var lines = await LoadLines<string>();
+        var banks = lines.Select(line => line.Select(char.GetNumericValue).Where(x => x >= 0).ToList()).ToList();
+        var joltage = CurrentOptions.SolutionType is SolutionType.First 
+            ? banks.Select(GetJoltage).Sum() 
+            : banks.Select(GetFullJoltage).Sum();
+        return joltage.ToString();
+    }
+
+    private static long GetJoltage(List<double> bank)
+    {
+        var bankValues = bank.OrderDescending().Distinct().ToList();
+        var biggestBatteryIndex = bank.IndexOf(bankValues.First());
+ 
+        if (biggestBatteryIndex == bank.Count - 1)
+            biggestBatteryIndex = bank.IndexOf(bankValues.Skip(1).First());
+
+        var secondBattery = bank.Skip(biggestBatteryIndex + 1).Max();
+        var biggestBattery = bank[biggestBatteryIndex];
+
+        return long.Parse(biggestBattery.ToString() + secondBattery.ToString());
+    }
+
+    private static long GetFullJoltage(List<double> bank)
+    {
+        var batteries = new List<double>();
+        var batteryAim = 12;
+        var lastBatteryIndex = -1;
+        while (batteries.Count < batteryAim)
+        {
+            var potentialBatteries = bank.Skip(lastBatteryIndex + 1).ToList();
+            var batteriesNeeded = batteryAim - batteries.Count;
+            var availableBatteryCount = potentialBatteries.Count - batteriesNeeded;
+            var availableBatteries = potentialBatteries.Take(availableBatteryCount + 1).ToList();
+            var indexOfBiggestBattery = availableBatteries.IndexOf(availableBatteries.Max());
+            var biggestBattery = availableBatteries[indexOfBiggestBattery];
+            batteries.Add(biggestBattery);
+            lastBatteryIndex = lastBatteryIndex + 1 + indexOfBiggestBattery;
+        }
+
+        var result = long.Parse(batteries.Select(x => x.ToString()).Aggregate((a, b) => a + b));
+        return result;
     }
 }
