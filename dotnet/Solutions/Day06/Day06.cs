ea84745fdf5bff8d5a843fa5f24800d6490f6f4e
Danny Wilson <danny@wlsn.xyz>
2025-12-06 18:22:07 +0000

    feat: Day 6 both solutions

--- dotnet/Solutions/Day06/Day06.cs
+++ dotnet/Solutions/Day06/Day06.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+using AdventOfCode.Helpers;
 using AdventOfCode.Models;
 using AdventOfCode.Services;
 using Microsoft.Extensions.Options;
@@ -10,6 +12,55 @@ public class Day06(IFileLoader fileLoader, IOptions<SolutionOptions> options) : 
 
     public override async Task<string> Solve()
     {
-        return "Hello, World!";
+        var lines = await LoadLines<string>();
+        var starts = Regex.Matches(lines[^1], @"\S")
+            .Cast<Match>()
+            .Select(m => m.Index)
+            .ToList();
+
+        var formatted = lines
+        .Select(l => starts
+            .Select((s, i) =>
+            {
+                var slice = l[s..(i + 1 < starts.Count ? starts[i + 1] : l.Length)];
+                return i + 1 < starts.Count && slice.EndsWith(' ')
+                    ? slice[..^1]
+                    : slice;
+            })
+            .ToList())
+        .ToList();
+
+        long total = 0;
+        var range = CurrentOptions.SolutionType is SolutionType.First
+            ? Enumerable.Range(0, formatted.First().Count)
+            : Enumerable.Range(0, formatted.First().Count).Reverse();
+        foreach (var column in range)
+        {
+            var columnValues = formatted.Select(line => line[column]).ToList();
+            var op = columnValues[^1];
+            List<long> values = [];
+            if (CurrentOptions.SolutionType is SolutionType.First)
+            {
+                values = [.. columnValues[..^1].Select(x => long.Parse(x.Trim()))];
+            }
+            else
+            {
+                var valueStrings = columnValues[..^1];
+                foreach (var col in Enumerable.Range(0, valueStrings.Select(x => x.Length).Max()).Reverse())
+                {
+                    var colValues = valueStrings
+                        .Select(x => x[col]).Where(x => x != ' ').ToList();
+                    values.Add(long.Parse(string.Join("", colValues)));
+                }
+            }
+
+            total += op.Trim() switch {
+                "+" => values.Sum(),
+                "*" => values.Aggregate((x, y) => x * y),
+                _ => throw new ArgumentException(nameof(op))
+            };
+        }
+        
+        return total.ToString();
     }
 }
