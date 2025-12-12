b337babcc196f33fd1aaaea976b09a56a236ab2c
Danny Wilson <danny@wlsn.xyz>
2025-12-12 14:57:45 +0000

    feat: Day 12 solution

--- dotnet/Solutions/Day12/Day12.cs
+++ dotnet/Solutions/Day12/Day12.cs
@@ -10,6 +10,29 @@ public class Day12(IFileLoader fileLoader, IOptions<SolutionOptions> options) : 
 
     public override async Task<string> Solve()
     {
-        return "Hello, World!";
+        var lines = await LoadLines<string>();
+        var presents = lines.TakeWhile(x => !x.Contains('x'))
+            .Where(x => !x.Contains(':'))
+            .Chunk(3)
+            .Select(x => x.ToList())
+            .ToList();
+        var spaces = lines.SkipWhile(x => !x.Contains('x'))
+            .Select(x => x.Split(":", StringSplitOptions.RemoveEmptyEntries))
+            .Select(x => (Size: x[0].Trim(), Presents: x[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList()))
+            .ToList();
+
+        var presentSizes = presents.Select(x => x.Sum(y => y.Count(c => c == '#'))).ToList();
+
+        int fitCount = 0;
+        foreach (var (Size, Presents) in spaces)
+        {
+            var totalSize = Presents.Select((p, i) => p * presentSizes[i]).Sum();
+            if (totalSize <= Size.Split('x').Select(int.Parse).Aggregate((a, b) => a * b))
+            {
+                fitCount++;
+            }
+        }
+
+        return fitCount.ToString();
     }
 }
